cdcf3b4baf39c44442a45f5ae527ce773e10de2b
stelmashchukyurii-maker <stelmashchukyurii@gmail.com>
2026-09-08 18:46:20 +0300

    Wire explicit Today refresh handler

--- trend.js
+++ trend.js
@@ -132,3 +132,4 @@ loadTrend();
 setInterval(loadTrend,60000);
 
 (()=>{const s=document.createElement('script');s.src='./mode.js?v=20260908-2';s.async=false;s.onload=()=>{if(document.readyState!=='loading'&&typeof hlInstallShell==='function'){hlInstallShell();hlInstallModeSwitch();hlSetMode(document.body.classList.contains('research-page')?'research':hlSavedMode());hlObserveSimple()}};document.head.appendChild(s)})();
+(()=>{const s=document.createElement('script');s.src='./today-refresh.js?v=20260908-1';s.async=false;document.head.appendChild(s)})();
