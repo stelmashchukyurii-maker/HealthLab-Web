65e3fde6a64b38262fe1e781bbd69c137f619874
stelmashchukyurii-maker <stelmashchukyurii@gmail.com>
2026-09-12 14:41:59 +0300

    Refresh Florivo Chat PWA cache to v0.1.1

--- florivo-chat/sw.js
+++ florivo-chat/sw.js
@@ -1,9 +1,9 @@
-const CACHE = "florivo-chat-v0.1.0";
+const CACHE = "florivo-chat-v0.1.1";
 const SHELL = [
   "./",
   "./index.html",
-  "./style.css?v=0.1.0",
-  "./app.js?v=0.1.0",
+  "./style.css?v=0.1.1",
+  "./app.js?v=0.1.1",
   "./manifest.webmanifest",
   "./icon-192.png",
   "./icon-512.png"
