63a93fd1a9aa31f53b3c92fff4a6c8d24e9c17ad
stelmashchukyurii-maker <stelmashchukyurii@gmail.com>
2026-09-12 22:27:09 +0300

    Florivo Chat v0.3.0 cache bump

--- florivo-chat/sw.js
+++ florivo-chat/sw.js
@@ -1,9 +1,9 @@
-const CACHE = "florivo-chat-v0.2.2";
+const CACHE = "florivo-chat-v0.3.0";
 const SHELL = [
   "./",
   "./index.html",
-  "./style.css?v=0.2.1",
-  "./app.js?v=0.2.2",
+  "./style.css?v=0.3.0",
+  "./app.js?v=0.3.0",
   "./manifest.webmanifest",
   "./icon-192.png",
   "./icon-512.png"
